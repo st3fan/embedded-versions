a92b7c4b8bd18eee093e8c9662bb49ebc26fada2
Stefan Arentz <stefan@arentz.ca>
2023-09-29 15:03:01 -0400

    Work in progress

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello, I am am Go program")
+	fmt.Printf("Hello, I am am Go program (version=%s commit=%s date=%s)\n", version, commit, date)
 }
---
+++ version.go
@@ -0,0 +1,7 @@
+package main
+
+var (
+	version = ""
+	commit  = ""
+	date    = ""
+)
